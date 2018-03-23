ec6900bad1d1df149032c622cfe10edb3f9a8707
allimonier.martin@gmail.com <allimonier.martin@gmail.com>
2018-03-23 14:26:07 +0100

    Adding comment on JS

--- sport_management/controllers/controllers.py
+++ sport_management/controllers/controllers.py
@@ -2,43 +2,15 @@
 from odoo import http
 from odoo.http import request
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import logging
 import json
 
 _logger = logging.getLogger(__name__)
 class SportController(http.Controller):
     
+    # controller that render our calendar page
     @http.route('/sport/sessions',auth='public', website=True, type="http")
     def index(self,**kw):
 
         env = request.env
         return http.request.render('sport_management.sport_calendar_view')
-
-
-
-    @http.route('/sport/get_session',auth='public', website=True, type="json")
-    def get_session(self,**kw):
-        env = request.env
-
-        lst_start = datetime.now()
-        month = relativedelta(months=1)
-        lst_end = lst_start + month
-
-        sessions = env['sport.session'].search([('status','like','valid'),('start_date','<=', str(lst_start)),('end_date','<=', str(lst_end))])
-
-        data = '{'
-        for session in sessions:
-            data += str(session.serialize_json())
-
-        data += '}'
-
-        return json.dumps(data)
-            
-
-    @http.route('/sport/test', auth='public', website=True, type="json")
-    def testJson(self,**kw):
-        
-        env = request.env
-        sessions = env['sport.session'].search([('status','like','valid')])
-        return {"nom" : "Allimonier", "prenom" : "Martin", "att" : "est un f****** Genie"}
--- sport_management/static/js/calendar.js
+++ sport_management/static/js/calendar.js
@@ -1,8 +1,7 @@
 window.onload = init;
 
 function init(){
-
-    console.log("init Calendar.js");
+    // launching calendar function
     calendar_printer();
 }
 
@@ -10,29 +9,32 @@ function init(){
 function calendar_printer(){
 
     odoo.define("sport.my_js", function(require) { 'use strict';
-
-        var ajax = require('web.ajax');
+        // loading odoo ressources
         var rpc = require('web.rpc');
         var weContext = require('web_editor.context');
 
+        // we are loading our sessions with odoo rpc api
         rpc.query({
             model : 'sport.session',
             method : 'search_read',
             context : weContext.get(),
         }).then(function(data){
             
+            // ou session are in data, we are parsing them to use them in our calendar
             var sessions = '[';
             var json_len = Object.keys(data).length;
             for(var i=0;i<json_len;i++){
                 var start = new Date(data[i].start_date);
                 var end = new Date(data[i].end_date);
+                // data format for fullcalendar : { "title" : title, "start" : start_date, "end" : end_date, "color" : color}
                 var session = '{"title":"'+ data[i].name +'","start":"'+start+'","end":"'+end+'","allDay":false,"color":"'+data[i].color+'"},';
                 sessions += session;
             }
             sessions = sessions.substring(0, sessions.length -1)
             sessions += "]";
             sessions = JSON.parse(sessions);
-
+            
+            // init calendar
             $('#calendar').fullCalendar({
                 height : 650,
                 themeSystem: 'bootstrap4',
