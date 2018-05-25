574b9e98eacd28fd5d93092a90b59b4bffe1c0eb
Martin Allimonier <allimonier.martin@gmail.com>
2018-05-25 15:07:32 +0200

    updating scan_card function to manage type_course

--- sport_management/models/badge.py
+++ sport_management/models/badge.py
@@ -22,30 +22,41 @@ class SportBadge(models.Model):
         for badge in self:
             badge.credit_count = badge.account_id.credit_count
 
+    #return integer as message :
+    #0 : sub valided
+    #1 : sub already valided
+    #2 : No credit on card
+    #3 : No session soon
     def scan_card(self):
         message = ""
         for badge in self:
             client = badge.client_id
+            account = badge.account_id
             now = datetime.now()
+            # hour = now + timedelta(hours=1)
             hour = now + timedelta(hours=24)
             lst_subscriptions = client.sub_ids
+            type_id = None
 
             for sub in lst_subscriptions:
                 date = datetime.strptime(sub.session_id.start_date, '%Y-%m-%d %H:%M:%S')
 
                 _logger.info(date <= hour)
                 if date >= now and date <= hour:
+                    type_id = sub.session_id.activity_id.course_type_id
+                    credit_id = self.env['sport.credit'].search([('account_id', '=', account.id),('type_id', '=', type_id.id),('number_actual','>', 0)], order='date_buy asc',limit=1)
                     if sub.state == "valid":
                         message = 1
                     else :
-                        if badge.credit_count > 0:
-                            badge.account_id.remove_credit()
+                        if not credit_id:
+                            message = 2
+                        else:
+                            credit_id.number_actual = credit_id.number_actual - 1
                             message = 0
                             sub.state = "valid"
-                        else:
-                            message = 2
                 else:
                     message = 3
+            _logger.info(message)
             return message
                 
                 
