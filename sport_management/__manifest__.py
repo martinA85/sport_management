5366ce34b34daebebc1f9dff1a3df21d5bf52c1f
Martin Allimonier <allimonier.martin@gmail.com>
2018-05-29 14:05:28 +0200

    changer dependancies

--- sport_management/__manifest__.py
+++ sport_management/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'website', 'calendar','contacts'],
+    'depends': ['base', 'sale', 'website', 'calendar'],
 
     # always loaded
     'data': [
