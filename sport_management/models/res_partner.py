4eb3a6230a5a43976e6383b5ffb0fb652de749a1
Martin Allimonier <allimonier.martin@gmail.com>
2018-05-29 11:39:27 +0200

    deleting card_count from res.partner

--- sport_management/models/res_partner.py
+++ sport_management/models/res_partner.py
@@ -11,7 +11,6 @@ class ResPartner(models.Model):
     credit_ids = fields.One2many('sport.credit', 'client_id')
     badge_ids = fields.One2many('sport.badge','client_id')
     sub_ids = fields.One2many('sport.subscription','client_id')
-    badge_count = fields.Integer(compute="_compute_badge_count")
     is_coach = fields.Boolean(default=False)
     account_id = fields.Many2one('sport.account', string="Compte", compute='_compute_account_id')
 
@@ -20,10 +19,3 @@ class ResPartner(models.Model):
         for partner in self:
             for badge in partner.badge_ids:
                 partner.account_id = badge.account_id
-
-    @api.depends('badge_ids')
-    def _compute_badge_count(self):
-        for partner in self:
-            partner.badge_count = 0
-            for badge in partner.badge_ids:
-                partner.badge_count += 1
